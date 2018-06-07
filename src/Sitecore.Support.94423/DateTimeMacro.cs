c94d5e98a2c9138acb2f1257dfc64f9e897f05c2
AnnaAkimova <aak@sitecore.net>
2018-06-07 13:31:56 +0300

    Init commit

---
+++ src/Sitecore.Support.94423/DateTimeMacro.cs
@@ -0,0 +1,20 @@
+﻿namespace Sitecore.Rules.RuleMacros
+{
+  using Sitecore;
+  using Sitecore.Diagnostics;
+  using Sitecore.Text;
+  using Sitecore.Web.UI.Sheer;
+  using System.Xml.Linq;
+
+  public class DateTimeMacro : IRuleMacro
+  {
+    public void Execute(XElement element, string name, UrlString parameters, string value)
+    {
+      Assert.ArgumentNotNull(element, "element");
+      Assert.ArgumentNotNull(name, "name");
+      Assert.ArgumentNotNull(parameters, "parameters");
+      Assert.ArgumentNotNull(value, "value");
+      SheerResponse.ShowModalDialog(new UrlString(UIUtil.GetUri("control:Sitecore.Shell.Applications.Dialogs.DateTimeSelector")).ToString(), "580px", "475px", string.Empty, true);
+    }
+  }
+}
---
+++ src/Sitecore.Support.94423/DateTimeSelector.cs
@@ -0,0 +1,29 @@
+﻿namespace Sitecore.Shell.Applications.Dialogs.DateTimeSelectors
+{
+  using Sitecore;
+  using Sitecore.Web.UI.HtmlControls;
+  using Sitecore.Web.UI.Pages;
+  using Sitecore.Web.UI.Sheer;
+  using System;
+  using System.Runtime.CompilerServices;
+
+  public class DateTimeSelector : DialogForm
+  {
+    protected override void OnLoad(EventArgs e)
+    {
+      base.OnLoad(e);
+      if (!Context.ClientPage.IsEvent)
+      {
+        this.Date.Value = DateUtil.IsoDateToServerTimeIsoDate(DateUtil.IsoNow);
+      }
+    }
+
+    protected override void OnOK(object sender, EventArgs args)
+    {
+      SheerResponse.SetDialogValue(this.Date.Value);
+      base.OnOK(sender, args);
+    }
+
+    protected DateTimePicker Date { get; set; }
+  }
+}
